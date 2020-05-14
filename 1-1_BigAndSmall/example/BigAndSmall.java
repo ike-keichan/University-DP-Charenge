ffce2097d19fc1305d7984479cd9ed73865aec11
ike_keichan <ikeike.keikeichan0908@gmail.com>
2020-05-14 12:50:04 +0900

    2020/05/14 2nd commit Update Program

--- 1-1_BigAndSmall/example/BigAndSmall.java
+++ 1-1_BigAndSmall/example/BigAndSmall.java
@@ -1,12 +1,34 @@
 package example;
 
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * ランダムな数字を0.5と比較した大小を出力するクラス。
+ */
 public class BigAndSmall extends Object
 {
+
 	/**
-	 * 発展プログラミング演習 練習問題1-1のメインプログラム。
+	 * 条件式の関数。argumentが0.5未満だとtrue。
+	 */
+	private Predicate<Double> checker = argument -> argument < 0.5;
+
+	/**
+	 * valueを出力する関数。
+	 */
+	Consumer<Double> valuePrint = (aDouble) -> { System.out.println("value:" + aDouble); };
+
+	/**
+	 * resultを出力する関数。
+	 */
+	Consumer<String> resultPrint = (aString) -> { System.out.println("result:" + aString); };
+
+	/**
+	 * 発展プログラミング演習 練習問題1-1の起動プログラム。
 	 * @param arguments 引数の文字列の配列
 	 */
-	public static void main(String[] arguments)
+	public void run(String[] arguments)
 	{
 		//　乱数の宣言
 		Double aDouble = Math.random();
@@ -15,14 +37,12 @@ public class BigAndSmall extends Object
 		String aString = new String();
 
 		//　生成した乱数が0.5と比較しての大小を判定
-		if(aDouble < 0.5){
-			aString = "Small";
-		}else {
-			aString = "Big";
-		}
+		aString = (checker.test(aDouble)) ?  "Small" : "Big" ;
 
 		// 判定結果を表示
-		System.out.println("value:" + aDouble);
-		System.out.println("result:" + aString);
+		valuePrint.accept(aDouble);
+		resultPrint.accept(aString);
+		
 	}
+
 }
