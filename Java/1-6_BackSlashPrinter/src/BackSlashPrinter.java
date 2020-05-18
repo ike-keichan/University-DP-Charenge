5649438aaba6816d3f5cf00b19a3e1c2f318e0bc
ike_keichan <ikeike.keikeichan0908@gmail.com>
2020-05-19 00:04:01 +0900

    2020/05/19 1st commit Update Program

---
+++ Java/1-6_BackSlashPrinter/src/BackSlashPrinter.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.stream.IntStream;
+
+/**
+ * ターミナル上に＼”バックスラッシュ”を描くクラス
+ */
+public class BackSlashPrinter extends Object
+{
+    /**
+     * 発展プログラミング演習 練習問題1-6の起動プログラム
+     * @param arguments 引数の文字列の配列
+     */
+    public void run(String[] arguments)
+    {
+        IntStream.range(1, 10).forEach( rowIndex -> {
+            IntStream.range(1, 10).forEach( lineIndex -> {
+                String aString = rowIndex == lineIndex ? "X" : ".";
+                System.out.printf(aString);
+            });
+            System.out.println();
+        });
+
+    }
+}
---
+++ Java/1-7_XPrinter/src/XPrinter.java
@@ -0,0 +1,31 @@
+package src;
+
+import java.util.function.BiPredicate;
+import java.util.stream.IntStream;
+
+/**
+ * ターミナル上にX”エックス”を描くクラス
+ */
+public class XPrinter extends Object
+{
+    /**
+	 * 条件式の関数 argumentが0.5未満だとtrue
+	 */
+    private BiPredicate<Integer, Integer> checker = (rowIndex, lineIndex) -> rowIndex == lineIndex || rowIndex + lineIndex == 10;
+
+    /**
+     * 発展プログラミング演習 練習問題1-7の起動プログラム
+     * @param arguments 引数の文字列の配列
+     */
+    public void run(String[] arguments)
+    {
+        IntStream.range(1, 10).forEach( rowIndex -> {
+            IntStream.range(1, 10).forEach( lineIndex -> {
+                String aString = (checker.test(rowIndex, lineIndex)) ? "X" : ".";
+                System.out.printf(aString);
+            });
+            System.out.println();
+        });
+        
+    }
+}
